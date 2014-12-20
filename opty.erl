fc8c1b88c174dd55b3e995e3f057ee87f0506cfe
vbeleuta <vbeleuta@users.noreply.github.com>
2014-12-20 20:48:37 +0000

    added do_read and do_write

--- opty.erl
+++ opty.erl
@@ -1,15 +1,28 @@
 -module(opty).
--export([start/4, stop/1]).
+-export([start/5, stop/1]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Updates: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
-start(Clients, Entries, Updates, Time) ->
+%start(Clients, Entries, Updates, Time) ->
+%    register(s, server:start(Entries)),
+%    L = startClients(Clients, [], Entries, Updates),
+%    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, 
+%    DURATION ~w s ~n", [Clients, Entries, Updates, Time]),
+%    timer:sleep(Time*1000),
+%    stop(L).
+
+%% Clients: Number of concurrent clients in the system
+%% Entries: Number of entries in the store
+%% Updates: Number of write operations per transaction
+%% Updates: Number of read operations per transaction
+%% Time: Duration of the experiment (in secs)
+start(Clients, Entries, Updates, Reads, Time) ->
     register(s, server:start(Entries)),
-    L = startClients(Clients, [], Entries, Updates),
-    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, 
-    DURATION ~w s ~n", [Clients, Entries, Updates, Time]),
+    L = startClients(Clients, [], Entries, Updates, Reads),
+    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, ~w READS PER TRANSACTION, 
+    DURATION ~w s ~n", [Clients, Entries, Updates, Reads, Time]),
     timer:sleep(Time*1000),
     stop(L).
 
@@ -18,10 +31,11 @@ stop(L) ->
     stopClients(L),
     s ! stop.
 
-startClients(0,L,_,_) -> L;
-startClients(Clients, L, Entries, Updates) ->
-    Pid = client:start(Clients, Entries, Updates, s),
-    startClients(Clients-1, [Pid|L], Entries, Updates).
+startClients(0,L,_,_,_) -> L;
+startClients(Clients, L, Entries, Updates, Reads) ->
+    %Pid = client:start(Clients, Entries, Updates, s),
+    Pid = client:start(Clients, Entries, Updates, Reads, s),
+    startClients(Clients-1, [Pid|L], Entries, Updates, Reads).
 
 stopClients([]) -> ok;
 stopClients([Pid|L]) ->
