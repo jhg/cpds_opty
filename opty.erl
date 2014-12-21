09a6afda2853f1134feb68f398d132e5f24b9473
Maria Gabriela Valdes <mgvaldesgraterol@gmail.com>
2014-12-21 17:13:19 +0100

    changes made to run server and clients in different nodes

--- opty.erl
+++ opty.erl
@@ -1,5 +1,5 @@
 -module(opty).
--export([start/5, stop/1]).
+-export([start/5, stop/1, start_server/1, start_clients/6, stop_sep/2]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
@@ -18,6 +18,17 @@
 %% Updates: Number of write operations per transaction
 %% Updates: Number of read operations per transaction
 %% Time: Duration of the experiment (in secs)
+start_server(Entries) ->
+  register(s, server:start(Entries)).
+
+start_clients(Clients, Entries, Updates, Reads, Time, ServerNode) ->
+  register(s, server:start(Entries)),
+  L = startClients_sep(Clients, [], Entries, Updates, Reads, ServerNode),
+  io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w UPDATES PER TRANSACTION, ~w READS PER TRANSACTION,
+    DURATION ~w s ~n", [Clients, Entries, Updates, Reads, Time]),
+  timer:sleep(Time*1000),
+  stop_sep(L, ServerNode).
+
 start(Clients, Entries, Updates, Reads, Time) ->
     register(s, server:start(Entries)),
     L = startClients(Clients, [], Entries, Updates, Reads),
@@ -26,10 +37,21 @@ start(Clients, Entries, Updates, Reads, Time) ->
     timer:sleep(Time*1000),
     stop(L).
 
+stop_sep(L, ServerNode) ->
+  io:format("Stopping...~n"),
+  stopClients(L),
+  {s, ServerNode} ! stop.
+
 stop(L) ->
     io:format("Stopping...~n"),
     stopClients(L),
     s ! stop.
+
+startClients_sep(0,L,_,_,_,_) -> L;
+startClients_sep(Clients, L, Entries, Updates, Reads, ServerNode) ->
+  %Pid = client:start(Clients, Entries, Updates, s),
+  Pid = client:start(Clients, Entries, Updates, Reads, {s, ServerNode}),
+  startClients_sep(Clients-1, [Pid|L], Entries, Updates, Reads, ServerNode).
 
 startClients(0,L,_,_,_) -> L;
 startClients(Clients, L, Entries, Updates, Reads) ->
