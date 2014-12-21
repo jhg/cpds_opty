0fcb46d2fe28a0645f40fa5570a63eedcbf0efe8
Maria Gabriela Valdes <mgvaldesgraterol@gmail.com>
2014-12-21 14:57:16 +0100

    making small changes to add construction of subset of entries for each client, and other small details.

--- client.erl
+++ client.erl
@@ -16,8 +16,10 @@ open(Name, Entries, Updates, Reads, Server, Total, Ok) ->
             ok;
         {transaction, Validator, Store} ->
             Handler = handler:start(self(), Validator, Store),
+            %%Defining subset of entries for client
+            Subset = choose_random_entries_subset(Entries, Entries/3, []),
             do_transactions(Name, Entries, Updates, Reads, Server, Handler,
-                            Total, Ok, Updates, Reads)
+                            Total, Ok, Updates, Reads, Subset)
     end.
 
 %start(Name, Entries, Updates, Server) ->
@@ -41,17 +43,17 @@ open(Name, Entries, Updates, Reads, Server, Total, Ok) ->
             %                Total, Ok, Updates, Subset)
 %    end.
 
-%choose_random_entries_subset(_, 0, Subset) ->
-%  Subset;
+choose_random_entries_subset(_, 0, Subset) ->
+  Subset;
 
-%choose_random_entries_subset(Entries, Size, Subset) ->
-%  Entry = random:uniform(Entries),
-%  case lists:member(Entry, Subset) of
-%    true ->
-%      choose_random_entries_subset(Entries, Size, Subset);
-%    false ->
-%      choose_random_entries_subset(Entries, Size-1, [Entry|Subset])
-%  end.
+choose_random_entries_subset(Entries, Size, Subset) ->
+  Entry = random:uniform(Entries),
+  case lists:member(Entry, Subset) of
+    true ->
+      choose_random_entries_subset(Entries, Size, Subset);
+    false ->
+      choose_random_entries_subset(Entries, Size-1, [Entry|Subset])
+  end.
 
 % Commit transaction
 %do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, 0, _) ->
@@ -78,7 +80,7 @@ open(Name, Entries, Updates, Reads, Server, Total, Ok) ->
 %    do_transactions(Name, Entries, Updates, Server, Handler, Total, Ok, N-1, Subset).
 
 % Commit transaction
-do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, 0, 0) ->
+do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, 0, 0, _) ->
     %io:format("~w: Commit: TOTAL ~w, OK ~w~n", [Name, Total, Ok]),
     %timer:sleep(Name*10),
     Ref = make_ref(),
@@ -92,55 +94,57 @@ do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, 0, 0)
     end;
 
 % Reads and Writes
-do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads) ->
+do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset) ->
     %io:format("~w: R/W: TOTAL ~w, OK ~w, N ~w~n", [Name, Total, Ok, N]),
     N = random:uniform(),
     if
         N >= 0.5 ->
-            do_read(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads);
+            do_read(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset);
         true ->
-            do_write(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads)
+            do_write(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset)
     end.
 
-do_read(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads) ->
+do_read(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset) ->
     if
         Nreads == 0 ->
-            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads);
+            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset);
         true ->
             Ref = make_ref(),
-            Num = random:uniform(Entries),
+            Num = choose_random_entry(Subset),
+%%             Num = random:uniform(Entries),
             Handler ! {read, Ref, Num},
-            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads-1)
+            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads-1, Subset)
     end.
 
-do_write(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads) ->
+do_write(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset) ->
     if
         Nwrites == 0 ->
-            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads);
+            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites, Nreads, Subset);
         true ->
             Ref = make_ref(),
-            Num = random:uniform(Entries),
+            Num = choose_random_entry(Subset),
+%%             Num = random:uniform(Entries),
             Handler ! {read, Ref, Num}, 
             % we need to read a value to write one or 
             % we can just make a random here and write a rand value 
             % if we dont want to do this additional read
             Value = receiveValue(Ref),
             Handler ! {write, Num, Value+1},
-            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites-1, Nreads)
+            do_transactions(Name, Entries, Updates, Reads, Server, Handler, Total, Ok, Nwrites-1, Nreads-1, Subset)
     end.
 
-%choose_random_entry(Subset) ->
-%  SubsetLength = length(Subset),
-%  Num = random:uniform(SubsetLength),
-%  get_entry_at_pos(Num, Subset, 1).
+choose_random_entry(Subset) ->
+  SubsetLength = length(Subset),
+  Num = random:uniform(SubsetLength),
+  get_entry_at_pos(Num, Subset, 1).
 
-%get_entry_at_pos(Pos, [H|T], CurrentPos) ->
-%  if
-%    Pos == CurrentPos ->
-%      H;
-%    true ->
-%      get_entry_at_pos(Pos, T, CurrentPos+1)
-%  end.
+get_entry_at_pos(Pos, [H|T], CurrentPos) ->
+  if
+    Pos == CurrentPos ->
+      H;
+    true ->
+      get_entry_at_pos(Pos, T, CurrentPos+1)
+  end.
 
 receiveCommitValue(Ref) ->
     receive
