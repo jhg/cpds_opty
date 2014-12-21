cedfe0938071ffdb7157ebe8efe644cb32adf378
vbeleuta <vbeleuta@users.noreply.github.com>
2014-12-21 01:43:06 +0000

    Update opty_sep.erl

--- opty_sep.erl
+++ opty_sep.erl
@@ -1,5 +1,5 @@
 -module(opty_sep).
--export([start_clients/5, start_server/1, stop/1]).
+-export([start_clients/5, start_server/2, stop/1]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
@@ -12,8 +12,8 @@ start_clients(Clients, Entries, Updates, Time, ServerNode) ->
     timer:sleep(Time*1000),
     stop(L).
 
-start_server(Entries) ->
-    server:start(Entries).
+start_server(Entries, ClientNode) ->
+    server_sep:start(Entries, ClientNode).
 
 stop(L) ->
     io:format("Stopping...~n"),
@@ -22,7 +22,7 @@ stop(L) ->
 
 startClients(0,L,_,_,_) -> L;
 startClients(Clients, L, Entries, Updates, ServerNode) ->
-    Pid = client:start(Clients, Entries, Updates, {s, ServerNode}),
+    Pid = client_sep:start(Clients, Entries, Updates, {s, ServerNode}),
     startClients(Clients-1, [Pid|L], Entries, Updates, ServerNode).
 
 stopClients([]) -> ok;
